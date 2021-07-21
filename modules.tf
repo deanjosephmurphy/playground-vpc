13a9157c57414494916c7ffe245050c81ccfcb94
Dean Murphy <dean.joseph.murphy@gmail.com>
2021-07-21 14:03:18 +0200

    modified modules.tf. expanded to 5 subnets then back down to 3

--- modules.tf
+++ modules.tf
@@ -7,10 +7,10 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   public_subnets = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
   private_subnets  = ["192.168.101.0/24", "192.168.102.0/24", "192.168.103.0/24"]
-
+  
   enable_nat_gateway = true
   single_nat_gateway  = false
-  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
+  reuse_nat_ips       = false                    
   external_nat_ip_ids = "${aws_eip.nat.*.id}"
 
   tags = {
